73e60475c97b16b6cb4dad72751872d817840dd1
sanjay-in <sanjaysridharan@outlook.com>
2025-11-18 08:54:14 +0530

    add liquidate and update main router function

--- src/Minter.sol
+++ src/Minter.sol
@@ -3,9 +3,11 @@ pragma solidity ^0.8.27;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {DecentralizedStableCoin} from "./DSC.sol";
+import {IRouter} from "./interface/IRouter.sol";
 
-contract Minter {
+contract Minter is Ownable {
     using SafeERC20 for IERC20;
 
     /// Errors ///
@@ -25,7 +27,7 @@ contract Minter {
     event DSCMinted(address indexed _user, uint256 indexed _amount);
     event DSCBurned(address indexed _user, uint256 indexed _amount);
 
-    constructor(address _mainRouter) {
+    constructor(address _mainRouter) Ownable(msg.sender) {
         i_dsc = new DecentralizedStableCoin("DecentralizedStableCoin", "DSC");
         s_mainRouter = _mainRouter;
     }
@@ -60,22 +62,22 @@ contract Minter {
 
     /** 
      * @notice Calls internal mint function
-     * @param _receiver who wants to mint the DSC to
+     * @param _to who wants to mint the DSC to
      * @param _amount of DSC to mint
      */
-    function mint(address _receiver, uint256 _amount) external checkZeroAddress(_receiver) checkZeroAmount(_amount) onlyMainRouter(msg.sender) {
-        _mint(_receiver, _amount);
+    function mint(address _to, uint256 _amount) external checkZeroAddress(_to) checkZeroAmount(_amount) onlyMainRouter(msg.sender) {
+        _mint(_to, _amount);
     }
 
     /**
      * @notice Mint function updates the minted value and mints DSC to the receiver
-     * @param _receiver who wants to mint the DSC to
+     * @param _to who wants to mint the DSC to
      * @param _amount of DSC to mint
      */
-    function _mint(address _receiver, uint256 _amount) internal {
-        s_minted[_receiver] += _amount;
-        i_dsc.mint(_receiver, _amount);
-        emit DSCMinted(_receiver, _amount);
+    function _mint(address _to, uint256 _amount) internal {
+        s_minted[_to] += _amount;
+        i_dsc.mint(_to, _amount);
+        emit DSCMinted(_to, _amount);
     }
 
     /**
@@ -100,6 +102,39 @@ contract Minter {
         emit DSCBurned(_user, _amount);
     }
 
+    /**
+     * @notice Liquidate a particular user's collateral who violates the health factor
+     * Checks for zero address of token and user and checks zero amount
+     * @param _userToLiquidate address of the user to liquidate
+     * @param _receiver address of the Depositor contract
+     * @param _token address of the token
+     * @param _amount of debt to cover
+     */
+    function liquidate(address _userToLiquidate, address _receiver, address _token, uint256 _amount) 
+        external 
+        payable 
+        checkZeroAmount(_amount) 
+        checkZeroAddress(_token)
+        checkZeroAddress(_userToLiquidate) 
+    {
+        s_userFee[msg.sender] += msg.value;
+        _liquidate(_userToLiquidate, _receiver, msg.sender, _token, _amount);
+    }
+
+    /**
+     * @notice Internal function called by liquidate function
+     * It burns the amount the user wants to cover and calls the liquidate function on the main router
+     * @param _userToLiquidate address of the user to liquidate
+     * @param _receiver address of the Depositor contract
+     * @param _sender address of the sender who wants to liquidate a user
+     * @param _token address of the token
+     * @param _amount of debt to cover
+     */
+    function _liquidate(address _userToLiquidate, address _receiver, address _sender, address _token, uint256 _amount) internal {
+        _burn(_sender, _amount);
+        IRouter(s_mainRouter).liquidate(_userToLiquidate, _receiver, _token, _amount, _sender);
+    }
+
     /// Getter Functions ///
 
     /**
@@ -116,5 +151,16 @@ contract Minter {
      */
     function getUserFee(address _user) external view returns(uint256) {
         return s_userFee[_user];
+    }
+
+
+    /// Setter Functions ///
+
+    /**
+     * Updates the main router address
+     * @param _mainRouter address of the new main router
+     */
+    function setMainRouter(address _mainRouter) external onlyOwner {
+        s_mainRouter = _mainRouter;
     }
 }
