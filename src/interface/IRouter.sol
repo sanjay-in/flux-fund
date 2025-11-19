f372e32401eec9bb567e4ebfe819ba21f9a57227
sanjay-in <sanjaysridharan@outlook.com>
2025-11-19 23:22:26 +0530

    fixes issues in interface

--- src/interface/IRouter.sol
+++ src/interface/IRouter.sol
@@ -2,11 +2,9 @@
 pragma solidity ^0.8.27;
 
 interface IRouter {
-    function liquidate(address _userToLiquidate, address _receiver, address _token, uint256 _amount) external;
+    function liquidate(address _userToLiquidate, address _receiver, address _token, uint256 _amount, address _sender) external;
 
-    function depositCollateral(address _depositor, address _tokenAddress, uint256 _amount) public;
+    function depositCollateral(address _depositor, address _tokenAddress, uint256 _amount) external;
     
     function depositAndMintTokens(address _tokenAddress, uint256 _amountToDeposit, address _receiver, address _depositor, uint256 _amountToMint) external;
-
-    function liquidate(address _userToLiquidate, address _receiver, address _token, uint256 _amount) external;
 }
